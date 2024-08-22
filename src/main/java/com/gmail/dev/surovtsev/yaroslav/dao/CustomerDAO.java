4bd7059cbf7db80f76c82898ac70cf5302587484
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2024-08-22 18:08:08 +0300

    Initial commit

--- src/main/java/com/gmail/dev/surovtsev/yaroslav/dao/CustomerDAO.java
+++ src/main/java/com/gmail/dev/surovtsev/yaroslav/dao/CustomerDAO.java
@@ -1,44 +1,46 @@
 package com.gmail.dev.surovtsev.yaroslav.dao;
 
 import com.gmail.dev.surovtsev.yaroslav.models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class CustomerDAO {
-    private int CUSTOMER_COUNT;
-    private List<Customer> customers;
 
-    {
-        customers = new ArrayList<>();
-        customers.add(new Customer(++CUSTOMER_COUNT, "Billy", "Dragon"));
-        customers.add(new Customer(++CUSTOMER_COUNT, "Jimmy", "Dragon"));
-        customers.add(new Customer(++CUSTOMER_COUNT, "Toad", "Frog"));
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public CustomerDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<Customer> index() {
-        return customers;
+        return jdbcTemplate.query("select * from customer", new BeanPropertyRowMapper<>(Customer.class));
     }
 
     public Customer show(Integer id) {
-        return customers.stream().filter(c -> c.getId().equals(id)).findFirst().orElse(null);
+        return jdbcTemplate.query("select * from customer where id = ?", new BeanPropertyRowMapper<>(Customer.class), id)
+                .stream().findFirst().orElse(null);
     }
 
     public void save(Customer customer) {
-        customer.setId(++CUSTOMER_COUNT);
-        customers.add(customer);
+        jdbcTemplate.update("insert into customer values(?, ?, ?)", getNextId(), customer.getFirstName(), customer.getLastName());
     }
 
     public void update(Integer id, Customer updatedCustomer) {
-        Customer customerToBeUpdated = show(id);
-        customerToBeUpdated.setFirstName(updatedCustomer.getFirstName());
-        customerToBeUpdated.setLastName(updatedCustomer.getLastName());
+        jdbcTemplate.update("update customer set first_name = ?, last_name = ? where id = ?", updatedCustomer.getFirstName(), updatedCustomer.getLastName(), id);
     }
 
     public void delete(Integer id) {
-        customers.removeIf(c -> c.getId().equals(id));
+        jdbcTemplate.update("delete from customer where id = ?", id);
+    }
+
+    private Integer getNextId() {
+        Integer lastId = jdbcTemplate.queryForObject("select max(id) from customer", Integer.class);
+        return lastId == null ? 1 : lastId + 1;
     }
 }
